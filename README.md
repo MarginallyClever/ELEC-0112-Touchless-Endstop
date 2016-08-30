57187db1b9ede6c1e8c1e43c9e5f71b537a37bcd
Dan Royer <dan@marginallyclever.com>
2016-08-30 14:53:52 -0700

    added code and readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# ELEC-0112 Touchless Endstop #
+
+Example code to blink a light on an Arduino when a touchless endstop is activated.
+
+Find the part, tutorials, and 3D models @ https://www.marginallyclever.com/product/touchless-3d-printer-endstop/
+
+2016-08-30 Lingkang Zhang <lingkangzhang@gmail.com>
---
+++ elec0112arduinoSketch/elec0112arduinoSketch.ino
@@ -0,0 +1,28 @@
+// Touchless 3D printer endstop example code
+// 2016-08-30 Lingkang Zhang <lingkangzhang@gmail.com>
+// www.marginallyclever.com
+
+int LED_PIN = 13;
+int IR_PIN = 9;
+
+
+void setup() {
+  // start serial 
+  Serial.begin(9600);
+  // get ready to blink the light
+  pinMode(LED_PIN, OUTPUT);
+  // get ready to read the sensor
+  pinMode(IR_PIN,INPUT);
+}
+
+
+void loop() {
+  // read the sensor
+  int sensorReading = digitalRead(IR_PIN);
+  
+  // print the sensor reading to the serial window
+  Serial.println(sensorReading);
+
+  // when sensorReading is zero there is nothing in the way.
+  digitalWrite(LED_PIN, (sensorReading>0) ? HIGH : LOW);
+}
